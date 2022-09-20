267fa3e4e0c20311fba5a37c77942f17094423e6
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2022-09-20 14:35:41 +0900

    Add files via upload

---
+++ CodeTest.py
@@ -0,0 +1,16 @@
+# 5-5 인접 행렬 방식
+import time
+start_time = time.time()
+
+INF = 99999999
+
+graph = [
+    [0, 7, 5],
+    [7, 0, INF],
+    [5, INF, 0]
+]
+
+print(graph)
+
+end_time = time.time()
+print("time : ", end_time - start_time)
