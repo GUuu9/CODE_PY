472f66623e5f70b9efbe62b3fac4655e027706db
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2022-09-20 10:20:18 +0900

    Add files via upload

---
+++ CodeTest.py
@@ -0,0 +1,27 @@
+# 4-1 상하좌우
+import time
+start_time = time.time()
+
+n = int(input())
+x, y = 1, 1
+tours = input().split()
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+for tour in tours:
+    for i in range(len(move_types)):
+        if tour == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+        if nx < 1 or ny < 1 or nx > n or ny > y:
+            continue
+        
+        x, y = nx, ny
+
+print(x, y)
+
+end_time = time.time()
+print("time : ", end_time - start_time)
