28bc1953d6faedaddacbaf0edd4ac553d1a8aeea
Uuu9 <87708661+GUuu9@users.noreply.github.com>
2022-12-05 18:54:56 +0900

    Update and rename CodeTest.py to 자료형 - 문자열 자료형.py

---
+++ 자료형 - 문자열 자료형.py
@@ -0,0 +1,15 @@
+### 자료형 ###
+
+## 문자열 자료형 ##
+# > 문자열 초기화 #
+data = 'Hello Wolrd'
+print(data)
+
+data = "Don't you Know \"Python\"?"
+print(data)
+
+# > 문자열 연산 #
+a = "Hello"
+b = "World"
+
+print(a + " " + b)
